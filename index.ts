35a679d90aa725f3cbd1b5e097ca6733e7139403
Jacob Bruce <jacobwbruce@hotmail.com>
2024-01-22 21:07:10 -0500

    typesafe client created

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
 import * as grpc from "@grpc/grpc-js";
 import * as protoLoader from "@grpc/proto-loader";
+import {
+  CalculatorClient,
+  CalculatorDefinition,
+} from "./proto/proto/calculator/Calculator";
+import { ProtoGrpcType } from "./proto/proto/calculator";
+import { CalculationRequest } from "./proto/proto/calculator/CalculationRequest";
 
 const PROTO_PATH = __dirname + "/proto/proto/calculator.proto";
 
@@ -13,18 +19,34 @@ const options = {
 
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
 
-const CalculatorService =
-  (grpc.loadPackageDefinition(packageDefinition).calculator as unknown) as ProtoGrpcType);
+const CalculatorService = grpc.loadPackageDefinition(packageDefinition)
+  .calculator as any;
 
 const client = new CalculatorService.Calculator(
   "localhost:8080",
   grpc.credentials.createInsecure(),
-);
+) as CalculatorClient;
 
-const then = performance.now();
-client.add({ a: 10, b: 20 }, async (err: any, response: any) => {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  console.log("Addition: ", response.result);
-  const now = performance.now();
-  console.log("Time taken in ms: ", now - then);
+let then = performance.now();
+client.Divide({ a: 10, b: 0 }, async (err, response) => {
+  if (err) {
+    console.log("Error: ", err);
+  }
+  if (response) {
+    console.log("Division: ", response.result);
+    const now = performance.now();
+    console.log("Time taken in ms: ", now - then);
+  }
+});
+
+then = performance.now();
+client.Sum({ numbers: [100, 100, 100] }, async (err: any, response: any) => {
+  if (err) {
+    console.log("Error: ", err);
+  }
+  if (response) {
+    console.log("Addition: ", response.result);
+    const now = performance.now();
+    console.log("Time taken in ms: ", now - then);
+  }
 });
