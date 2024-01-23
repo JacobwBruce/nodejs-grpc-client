adbe1027f7678ae9717744fd3cb24e004f819f1d
Jacob Bruce <jacobwbruce@hotmail.com>
2024-01-22 20:28:54 -0500

    grpc client

--- index.ts
+++ index.ts
@@ -1,12 +1,30 @@
-import { Client, credentials } from "@grpc/grpc-js";
+import * as grpc from "@grpc/grpc-js";
+import * as protoLoader from "@grpc/proto-loader";
 
-const client = new Client("localhost:8080", credentials.createInsecure());
+const PROTO_PATH = __dirname + "/proto/proto/calculator.proto";
 
-client.waitForReady(5000, (err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+const options = {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+};
 
-  client.close();
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
+
+const CalculatorService =
+  (grpc.loadPackageDefinition(packageDefinition).calculator as unknown) as ProtoGrpcType);
+
+const client = new CalculatorService.Calculator(
+  "localhost:8080",
+  grpc.credentials.createInsecure(),
+);
+
+const then = performance.now();
+client.add({ a: 10, b: 20 }, async (err: any, response: any) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  console.log("Addition: ", response.result);
+  const now = performance.now();
+  console.log("Time taken in ms: ", now - then);
 });
